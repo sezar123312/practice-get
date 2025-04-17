b8d6da992eb07574886aa07f071acb508257427f
sezar123312 <sezar554433@gmail.com>
2025-04-17 20:39:46 +0330

    comitte34

---
+++ projali.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void coutstudentId() {
+	cout << "student ID : 40002 " << endl;
+}
+int main (){
+cout<<"Hello World"<<endl;
+coutstudentID();
+return 0;
+
+}
