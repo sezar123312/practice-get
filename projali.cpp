bc9f5b2b2f20397dbc21dcbd198fd581cd2d18d2
sezar123312 <sezar554433@gmail.com>
2025-04-17 20:23:04 +0330

    Update projali.cpp
    Commit: Second Commit

--- projali.cpp
+++ projali.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
+void coutstudentId() {
+	cout << "student ID : 4033633002 " << endl;
+}
 int main (){
 cout<<"Hello World"<<endl;
+coutstudentID();
 return 0;
 
 }
